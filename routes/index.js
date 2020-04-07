46d68501325a129cc516b327577991d3fda66fbc
stuart lamble <lamble.stuart@googlemail.com>
2020-04-07 21:07:01 +0100

    LBB-2, 3, 4, 5 added back end api routes, to link to controller methods

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const router = require("express").Router()
+const taskController = require("../controllers/taskController")
+const path = require("path")
+
+//routes for new, current incomplete tasks
+
+router.route("/current")
+.get(taskController.findAll)
+.post(taskController.create)
+
+//routes for finding complete tasks
+ router.route("/saved")
+ .get(taskController.findAll)
+
+ //routes for updating tasks
+ router.route("current/:id")
+ .put(taskController.update)
+
+ // For anything else, render the html page
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  });
+
+ module.exports = router
