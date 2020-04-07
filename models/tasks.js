a37cdb8407a447676d19053bcc0add4cfd7309f3
stuart lamble <lamble.stuart@googlemail.com>
2020-04-07 19:34:45 +0100

    LBB-2 created models folder, and schema for tasks

---
+++ models/tasks.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    task:{
+        type: String,
+        required: true
+    },
+    subtasks:{
+        type: [Object]
+    },
+    notes:{
+        type: String
+    },
+    complete:{
+        type: Boolean,
+        default: false
+    }
+})
+
+const Tasks = mongoose.model("Tasks", taskSchema)
+
+module.exports = Tasks
